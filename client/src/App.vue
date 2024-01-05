fb1c9957b8732fed86e9ab7cf0804e677cfc1c3f
Ian Campbell <10663545+iancampbellstuff@users.noreply.github.com>
2024-01-05 01:31:33 -0500

    client App Vue file clearing service workers

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,3 +8,16 @@
 #q-app {
 }
 </style>
+
+<script setup lang="ts">
+// clear service workers
+if (navigator.serviceWorker) {
+    void navigator.serviceWorker
+        .getRegistrations()
+        .then((serviceWorkerRegistrations) => {
+            for (const serviceWorkerRegistration of serviceWorkerRegistrations) {
+                void serviceWorkerRegistration.unregister();
+            }
+        });
+}
+</script>
